e0f57848c0ae20e2ccb37912ce6dd5f5d0fa1328
amhughes617 <amhughes@g.cofc.edu>
2016-06-08 18:04:14 -0400

    added epically awesome styling

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,6 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?,us&appid=${AP
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;//static country code
   const request = axios.get(url);
